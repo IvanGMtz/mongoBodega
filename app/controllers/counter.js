004251641f1566876285bcc878b294718662e5d1
IvanGMtz <navi.garces24@gmail.com>
2023-08-16 21:09:57 -0500

    feat: :sparkles: Completed function counter autoincremental id

--- app/controllers/counter.js
+++ app/controllers/counter.js
@@ -1,12 +1,16 @@
 import { con } from "../../config/connection.js";
 
 export const siguienteId = async (coleccion) => {
-  let db = await con();
+  try {
+    let db = await con();
   const sequenceDocument = await db.collection("counters").findOneAndUpdate(
     { id: `${coleccion}Id` },
     { $inc: { sequence_value: 1 } },
     { returnDocument: "after" }
   );
-
-  return sequenceDocument.sequence_value;
+    return sequenceDocument.value.sequence_value;
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
+  }
 };
