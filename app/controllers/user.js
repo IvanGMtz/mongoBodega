dac08286e20629563a164f9313bdada9e6bb15cc
IvanGMtz <navi.garces24@gmail.com>
2023-08-16 21:10:27 -0500

    feat: :sparkles: Add complete CRUD to the table users

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -11,12 +11,13 @@ export const getUsers = async (req, res)=>{
 
 export const getUser = async (req, res)=>{
     if (!req.rateLimit) return;
-    let id = req.params.id
+    const id = Number(req.params.id);
     let db = await con();
     let collection = db.collection("users");
-    let result = await collection.find({"ID":id}).toArray();
+    let result = await collection.find({"id":id}).toArray();
     res.send(result);
 }
+
 export const addUser = async (req, res) => {
     if (!req.rateLimit) return;
     const {NOMBRE, EMAIL, ESTADO, PASSWORD} = req.body;
@@ -24,8 +25,6 @@ export const addUser = async (req, res) => {
     let collection = db.collection("users");
 
     const newUserId = await siguienteId("users");
-    console.log(newUserId);
-
     const newUser = {
         id: newUserId,
         nombre: NOMBRE,
@@ -33,11 +32,74 @@ export const addUser = async (req, res) => {
         estado: ESTADO,
         password: PASSWORD
     };
-
+    
     try {
       const result = await collection.insertOne(newUser);
       res.status(201).json({ message: "User added successfully", insertedId: result.insertedId });
     } catch (error) {
       res.status(500).json({ message: "Error adding user", error: error.message });
     }
+}
+
+export const deleteUser = async (req, res) => {
+  if (!req.rateLimit) return;
+  const userIdToDelete = Number(req.params.id);
+  if (!userIdToDelete) {
+    res.status(400).json({ message: "User ID not provided" });
+    return;
+  }
+
+  let db = await con();
+  let collection = db.collection("users");
+
+  try {
+    const result = await collection.deleteOne({ id: userIdToDelete });
+    if (result.deletedCount === 1) {
+      res.status(200).json({ message: "User deleted successfully" });
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Error deleting user", error: error.message });
+  }
+}
+
+export const updateUser = async (req, res) => {
+  if (!req.rateLimit) return;
+  const {NOMBRE, EMAIL, ESTADO, PASSWORD} = req.body;
+  const userIdToUpdate = Number(req.params.id);
+  if (!userIdToUpdate) {
+    res.status(400).json({ message: "User ID not provided" });
+    return;
+  }
+
+  const updatedUserData = {
+    id: Number(req.params.id),
+    nombre: NOMBRE,
+    email: EMAIL,
+    estado: ESTADO,
+    password: PASSWORD
+};
+  if (!updatedUserData) {
+    res.status(400).json({ message: "Updated user data not provided" });
+    return;
+  }
+
+  let db = await con();
+  let collection = db.collection("users");
+
+  try {
+    const result = await collection.updateOne(
+      { id: userIdToUpdate },
+      { $set: updatedUserData }
+    );
+
+    if (result.matchedCount === 1) {
+      res.status(200).json({ message: "User updated successfully" });
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Error updating user", error: error.message });
+  }
 }
--- app/routers/user.js
+++ app/routers/user.js
@@ -1,11 +1,13 @@
 import express from "express";
 import {limitGet} from "../middlewares/limit.js";
-import {getUsers, getUser, addUser} from "../controllers/user.js"
+import {getUsers, getUser, addUser, deleteUser, updateUser} from "../controllers/user.js"
 
 const appuser = express.Router();
 
 appuser.get("/", limitGet(), getUsers);
 appuser.post("/", limitGet(), addUser);
 appuser.get("/:id", limitGet(), getUser);
+appuser.put("/:id", limitGet(), updateUser);
+appuser.delete("/:id", limitGet(), deleteUser);
 
 export default appuser;
