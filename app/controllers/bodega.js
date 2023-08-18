c3b3455847d96ffd4c3192f64ebd9fe2b1554bc1
IvanGMtz <navi.garces24@gmail.com>
2023-08-18 10:15:47 -0500

    feat: :sparkles: Update endpoint bodega

--- app/controllers/bodega.js
+++ app/controllers/bodega.js
@@ -1,42 +1,62 @@
-import {con} from "../../config/connection.js";
-import {siguienteId} from "./counter.js";
+import { con } from "../../config/connection.js";
+import { siguienteId } from "./counter.js";
 
-export const getBodegas = async (req, res)=>{
-    if (!req.rateLimit) return;
-    let db = await con();
-    let collection = db.collection("bodegas");
-    let result = await collection.find({}).sort({ nombre: 1 }).toArray();
-    res.send(result);
+let db = await con();
+let collection = db.collection("bodegas");
+
+export const getBodegas = async (req, res) => {
+  if (!req.rateLimit) return;
+  let result = await collection.find({}).sort({ nombre: 1 }).toArray();
+  res.send(result);
 }
 
-export const getBodega = async (req, res)=>{
-    if (!req.rateLimit) return;
-    const id = Number(req.params.id);
-    let db = await con();
-    let collection = db.collection("bodegas");
-    let result = await collection.find({"id":id}).toArray();
-    res.send(result);
+export const getBodega = async (req, res) => {
+  if (!req.rateLimit) return;
+  const id = Number(req.params.id);
+  let result = await collection.find({ "id": id }).toArray();
+  res.send(result);
 }
 
 export const addBodega = async (req, res) => {
-    if (!req.rateLimit) return;
-    const {NOMBRE, ID_RESPONSABLE, ESTADO, CREATE_BY, CREATE_AT} = req.body;
-    let db = await con();
-    let collection = db.collection("bodegas");
-    const newbodegaId = await siguienteId("bodegas");    
-    try {
-      const result = await collection.insertOne({
-        id: newbodegaId,
-        nombre: NOMBRE,
-        id_responsable: ID_RESPONSABLE,
-        estado: ESTADO,
-        created_by: CREATE_BY,
-        created_at: new Date(CREATE_AT)
-    });
-      res.status(201).json({ message: "Bodega added successfully", insertedId: result.insertedId });
-    } catch (error) {
-      res.status(500).json({ message: "Error adding bodega", error: error.message });
+  if (!req.rateLimit) return;
+
+  const requiredFields = [
+    { field: "NOMBRE", message: "User NOMBRE not provided" },
+    { field: "ID_RESPONSABLE", message: "User ID_RESPONSABLE not provided" },
+    { field: "ESTADO", message: "User ESTADO not provided" }
+  ];
+
+  for (const { field, message } of requiredFields) {
+    if (req.body[field] === undefined) {
+      return res.status(400).json({ message });
     }
+  }
+
+  const {
+    NOMBRE, ID_RESPONSABLE, ESTADO,
+    CREATE_BY = 0,
+    UPDATE_BY = 0,
+    CREATE_AT = "0000-00-00",
+    UPDATE_AT = "0000-00-00",
+    DELETE_AT = "0000-00-00"
+  } = req.body;
+  const newbodegaId = await siguienteId("bodegas");
+  try {
+    const result = await collection.insertOne({
+      id: newbodegaId,
+      nombre: NOMBRE,
+      id_responsable: ID_RESPONSABLE,
+      estado: ESTADO,
+      created_by: CREATE_BY,
+      update_by: UPDATE_BY,
+      created_at: new Date(CREATE_AT),
+      updated_at: new Date(UPDATE_AT),
+      deleted_at: new Date(DELETE_AT)
+    });
+    res.status(201).json({ message: "Bodega added successfully", insertedId: result.insertedId });
+  } catch (error) {
+    res.status(500).json({ message: "Error adding bodega", error: error.errInfo.details.schemaRulesNotSatisfied[0].propertiesNotSatisfied[0].description });
+  }
 }
 
 export const deleteBodega = async (req, res) => {
@@ -64,7 +84,26 @@ export const deleteBodega = async (req, res) => {
 
 export const updateBodega = async (req, res) => {
   if (!req.rateLimit) return;
-  const {NOMBRE, ID_RESPONSABLE, ESTADO, CREATE_BY, CREATE_AT} = req.body;
+  const requiredFields = [
+    { field: "NOMBRE", message: "User NOMBRE not provided" },
+    { field: "ID_RESPONSABLE", message: "User ID_RESPONSABLE not provided" },
+    { field: "ESTADO", message: "User ESTADO not provided" }
+  ];
+
+  for (const { field, message } of requiredFields) {
+    if (req.body[field] === undefined) {
+      return res.status(400).json({ message });
+    }
+  }
+
+  const {
+    NOMBRE, ID_RESPONSABLE, ESTADO,
+    CREATE_BY = 0,
+    UPDATE_BY = 0,
+    CREATE_AT = "0000-00-00",
+    UPDATE_AT = "0000-00-00",
+    DELETE_AT = "0000-00-00"
+  } = req.body;
   const bodegaIdToUpdate = Number(req.params.id);
   if (!bodegaIdToUpdate) {
     res.status(400).json({ message: "Bodega ID not provided" });
@@ -77,8 +116,11 @@ export const updateBodega = async (req, res) => {
     id_responsable: ID_RESPONSABLE,
     estado: ESTADO,
     created_by: CREATE_BY,
-    created_at: new Date(CREATE_AT)
-};
+    update_by: UPDATE_BY,
+    created_at: new Date(CREATE_AT),
+    updated_at: new Date(UPDATE_AT),
+    deleted_at: new Date(DELETE_AT)
+  };
   if (!updatedbodegaData) {
     res.status(400).json({ message: "Updated bodega data not provided" });
     return;
--- app/routers/bodega.js
+++ app/routers/bodega.js
@@ -1,13 +1,10 @@
 import express from "express";
 import {limitGet} from "../middlewares/limit.js";
-import {getBodegas, getBodega, addBodega, deleteBodega, updateBodega} from "../controllers/bodega.js"
+import {getBodegas, addBodega} from "../controllers/bodega.js"
 
 const appBodega = express.Router();
 
 appBodega.get("/", limitGet(), getBodegas);
 appBodega.post("/", limitGet(), addBodega);
-appBodega.get("/:id", limitGet(), getBodega);
-appBodega.put("/:id", limitGet(), updateBodega);
-appBodega.delete("/:id", limitGet(), deleteBodega);
 
 export default appBodega;
