17f3c92c6b86fa826f7bb7617ecf64b96e0be045
IvanGMtz <navi.garces24@gmail.com>
2023-08-18 11:17:46 -0500

    feat: :sparkles: Update endpoint inventario

--- app/controllers/inventario.js
+++ app/controllers/inventario.js
@@ -77,15 +77,19 @@ export const updateInventario = async (req, res) => {
 
 export const insertOrUpdateInventario = async (req, res) => {
     if (!req.rateLimit) return;
+    const requiredFields = [
+      { field: "ID_BODEGA", message: "User ID_BODEGA not provided" },
+      { field: "ID_PRODUCTO", message: "User ID_PRODUCTO not provided" },
+      { field: "CANTIDAD", message: "User CANTIDAD not provided" }
+    ];
   
-    const { ID_BODEGA, ID_PRODUCTO } = req.body;
-    let { CANTIDAD } = req.body;
-    CANTIDAD = CANTIDAD ?? 10;
-  
-    if (!ID_BODEGA || !ID_PRODUCTO) {
-      res.status(400).json({ message: "Missing required fields" });
-      return;
+    for (const { field, message } of requiredFields) {
+      if (req.body[field] === undefined) {
+        return res.status(400).json({ message });
+      }
     }
+  
+    const { ID_BODEGA, ID_PRODUCTO, CANTIDAD, CREATE_BY} = req.body;
   
     try {
       const existingInventario = await collection.findOne({
@@ -101,19 +105,21 @@ export const insertOrUpdateInventario = async (req, res) => {
         );
         res.status(200).json({ message: "Inventario updated successfully" });
       } else {
+        if (CREATE_BY === undefined) {
+          return res.status(400).json({ message: "User CREATE_BY not provided" });
+        }
         const newInventarioId = await siguienteId("inventarios");
         await collection.insertOne({
           id: newInventarioId,
           id_bodega: ID_BODEGA,
           id_producto: ID_PRODUCTO,
           cantidad: CANTIDAD,
-          created_by: req.body.CREATE_BY,
-          created_at: req.body.CREATE_AT
+          created_by: CREATE_BY
         });
         res.status(201).json({ message: "Inventario added successfully" });
       }
     } catch (error) {
-      res.status(500).json({ message: "Error inserting/updating inventario", error: error.message });
+      res.status(500).json({ message: "Error inserting/updating inventario", error: error.errInfo.details.schemaRulesNotSatisfied[0].propertiesNotSatisfied[0].description });
     }
 }
   
--- app/routers/inventario.js
+++ app/routers/inventario.js
@@ -1,13 +1,11 @@
 import express from "express";
 import {limitGet} from "../middlewares/limit.js";
-import {getInventarios, getInventario, insertOrUpdateInventario, deleteInventario, updateInventario} from "../controllers/inventario.js"
+import {getInventarios, getInventario, insertOrUpdateInventario} from "../controllers/inventario.js"
 
 const appInventario = express.Router();
 
 appInventario.get("/", limitGet(), getInventarios);
 appInventario.post("/", limitGet(), insertOrUpdateInventario);
 appInventario.get("/:id", limitGet(), getInventario);
-appInventario.put("/:id", limitGet(), updateInventario);
-appInventario.delete("/:id", limitGet(), deleteInventario);
 
 export default appInventario;
